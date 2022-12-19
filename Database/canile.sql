dc4dcbe4352c6d18ce53f0d0bf7d5b388c899e58
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2022-12-19 16:56:51 +0100

    database modificato e aggiornato

---
+++ Database/canile.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Dic 19, 2022 alle 16:54
+-- Versione del server: 10.4.25-MariaDB
+-- Versione PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `canile`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `cani`
+--
+
+CREATE TABLE `cani` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `razza` varchar(30) NOT NULL,
+  `eta` int(11) NOT NULL,
+  `sesso` varchar(1) NOT NULL,
+  `sterilizzazione` tinyint(1) NOT NULL,
+  `vaccini` varchar(255) NOT NULL,
+  `mircrochip` tinyint(1) NOT NULL,
+  `descrizione` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `cani`
+--
+ALTER TABLE `cani`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `microchip` (`mircrochip`),
+  ADD KEY `sesso` (`sesso`),
+  ADD KEY `steriliz` (`sterilizzazione`),
+  ADD KEY `vaccino` (`vaccini`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `cani`
+--
+ALTER TABLE `cani`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
