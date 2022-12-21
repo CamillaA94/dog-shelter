8388ed4d60db796463ffe41b2f7b126c5263928d
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2022-12-21 09:56:22 +0100

    aggiunto controller login (vuoto)

---
+++ src/main/java/it/corso/controller/LoginController.java
@@ -0,0 +1,9 @@
+package it.corso.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+}
