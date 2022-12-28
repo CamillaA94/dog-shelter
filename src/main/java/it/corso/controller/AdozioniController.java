8fa4fd569cde923f4db6a3688e656ab9479f2896
GianlucaEvangelista <gianlucaevangelista95@gmail.com>
2022-12-28 18:00:22 +0100

    aggiunto Thymeleaf a adozioni

--- src/main/java/it/corso/controller/AdozioniController.java
+++ src/main/java/it/corso/controller/AdozioniController.java
@@ -2,9 +2,13 @@ package it.corso.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,15 +16,16 @@ import it.corso.model.Cane;
 import it.corso.service.CaneService;
 
 @Controller
-@RequestMapping("/adozione")
-public class AdozioneController {
+@RequestMapping("/adozioni")
+public class AdozioniController {
 
- private CaneService caneService;
+	@Autowired
+	private CaneService caneService;
     
     @GetMapping
-    public String getPage(Model model, @RequestParam(name = "ord", required = false) String ord) {
-    	List<Cane> cani = new ArrayList<Cane>();
-    	if(ord == null || ord.equals("alfabetico")) {
+    public String getPage(Model model, HttpSession session, @RequestParam(name = "ord", required = false) String ord) {
+		List<Cane> cani = new ArrayList<Cane>();
+		if(ord == null || ord.equals("alfabetico")) {
 			cani = caneService.getCaniAlfabetico();
 		} else if(ord.equals("ultimi")) {
 			cani = caneService.getCaniUltimi();
@@ -35,15 +40,20 @@ public class AdozioneController {
 		} else if(ord.equals("maschi")) {
 			cani = caneService.getCaniMaschi();
 		}
-    	String ordine;
+		String ordine;
 		if (ord == null) {
 			ordine = "alfabetico";
 		} else {
 			ordine = ord;
 		}
-    	model.addAttribute("titolo", "Adozione");
-        model.addAttribute("cani", cani);
-        model.addAttribute("ordine", ordine);
-        return "adozione";
+    	model.addAttribute("titolo", "Adozioni");
+		model.addAttribute("cani", cani);
+		model.addAttribute("ordine", ordine);
+		return "adozioni";
     }
+    
+    @PostMapping("/ordina")
+	public String ordinaCani(@RequestParam("ordine") String ordine) {
+		return "redirect:/adozioni?ord=" + ordine;
+	}
 }
